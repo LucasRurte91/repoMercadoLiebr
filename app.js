8eba4d6e011eb273fedfbc5c8ac69ba24e5a4387
Lucas Ruarte91 <lucasruarte688@gmail.com>
2021-09-20 23:47:22 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require ("express");
+const app = express();
+const path = require ("path");
+app.use(express.static("public"));
+const PORT = process.env.PORT || 3000;
+
+
+app.listen (process.env.PORT || 3000, function (){
+    console.log("servidor corriendo en el puerto 3000");
+})
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"./views/home.html"))
+});
+
+app.get("/login",(req, res) => { 
+    res.sendFile(path.join(__dirname,"./views/login.html"))
+});
+app.get("/register", function (req, res) {
+    res.sendFile(path.join(__dirname, "/views", "/register.html"));
+  });
